ca19b6b7f11865718aba4c2a263ba48a5c426162
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-02-08 07:44:30 -0800

    refactor(configuration-types): rename DnsRule

--- configuration/configuration-types/dns.go
+++ configuration/configuration-types/dns.go
@@ -1,12 +1,12 @@
 package configuration_types
 
-type DnsRule struct {
+type dnsRule struct {
 	Address  string   `json:"address"`
 	Domains  []string `json:"domains"`
 	GeoSites []string `json:"geoSites"`
 }
 
 type DnsItem struct {
-	Local  DnsRule `json:"local"`
-	Remote DnsRule `json:"remote"`
+	Local  dnsRule `json:"local"`
+	Remote dnsRule `json:"remote"`
 }
