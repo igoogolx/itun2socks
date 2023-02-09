e4a9606ea14c9ce21d130fb1dce10f3fa6fc46f9
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-02-08 18:38:36 -0800

    refactor(proxy-handler): rename modules

--- components/proxy-handler/handler.go
+++ components/proxy-handler/handler.go
@@ -3,9 +3,9 @@ package proxy_handler
 import (
 	"context"
 	"github.com/Dreamacro/clash/log"
-	conn2 "github.com/igoogolx/itun2socks/conn"
-	tunnel2 "github.com/igoogolx/itun2socks/tunnel"
-	buf2 "github.com/sagernet/sing/common/buf"
+	"github.com/igoogolx/itun2socks/conn"
+	"github.com/igoogolx/itun2socks/tunnel"
+	"github.com/sagernet/sing/common/buf"
 	M "github.com/sagernet/sing/common/metadata"
 	"github.com/sagernet/sing/common/network"
 	"net"
@@ -16,35 +16,35 @@ type udpConn struct {
 	network.PacketConn
 }
 
-func (uc udpConn) ReadFrom(buf []byte) (int, net.Addr, error) {
-	newBuf := buf2.NewPacket()
+func (uc udpConn) ReadFrom(data []byte) (int, net.Addr, error) {
+	newBuf := buf.NewPacket()
 	defer newBuf.Release()
 	des, err := uc.ReadPacket(newBuf)
 	if err != nil {
 		return 0, nil, err
 	}
-	n, err := newBuf.Read(buf)
+	n, err := newBuf.Read(data)
 	if err != nil {
 		return 0, nil, err
 	}
 	return n, des.UDPAddr(), nil
 }
 
-func (uc udpConn) WriteTo(buf []byte, addr net.Addr) (int, error) {
-	newBuf := buf2.NewPacket()
-	_, err := newBuf.Write(buf)
+func (uc udpConn) WriteTo(data []byte, addr net.Addr) (int, error) {
+	newBuf := buf.NewPacket()
+	_, err := newBuf.Write(data)
 	defer newBuf.Release()
 	if err != nil {
 		return 0, err
 	}
 	err = uc.WritePacket(newBuf, M.SocksaddrFromNet(addr))
-	return len(buf), err
+	return len(data), err
 }
 
 type ConnHandler struct {
 }
 
-func (c ConnHandler) NewConnection(ctx context.Context, conn net.Conn, metadata M.Metadata) error {
+func (c ConnHandler) NewConnection(ctx context.Context, netConn net.Conn, metadata M.Metadata) error {
 	local, err := net.ResolveTCPAddr("tcp", metadata.Source.String())
 	if err != nil {
 		return err
@@ -53,16 +53,16 @@ func (c ConnHandler) NewConnection(ctx context.Context, conn net.Conn, metadata 
 	if err != nil {
 		return err
 	}
-	m := tunnel2.CreateTcpMetadata(*local, *remote)
+	m := tunnel.CreateTcpMetadata(*local, *remote)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	defer wg.Wait()
-	ct := conn2.NewTcpConnContext(ctx, conn, &m, &wg)
-	tunnel2.AddTcpConn(*ct)
+	ct := conn.NewTcpConnContext(ctx, netConn, &m, &wg)
+	tunnel.AddTcpConn(*ct)
 	return nil
 }
 
-func (c ConnHandler) NewPacketConnection(ctx context.Context, conn network.PacketConn, metadata M.Metadata) error {
+func (c ConnHandler) NewPacketConnection(ctx context.Context, packetConn network.PacketConn, metadata M.Metadata) error {
 	local, err := net.ResolveUDPAddr("udp", metadata.Source.String())
 	if err != nil {
 		return err
@@ -71,15 +71,15 @@ func (c ConnHandler) NewPacketConnection(ctx context.Context, conn network.Packe
 	if err != nil {
 		return err
 	}
-	m := tunnel2.CreateUdpMetadata(*local, *remote)
+	m := tunnel.CreateUdpMetadata(*local, *remote)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	defer wg.Wait()
-	ct, err := conn2.NewUdpConnContext(ctx, udpConn{conn}, &m, &wg)
+	ct, err := conn.NewUdpConnContext(ctx, udpConn{packetConn}, &m, &wg)
 	if err != nil {
 		return err
 	}
-	tunnel2.AddUdpConn(ct)
+	tunnel.AddUdpConn(ct)
 	return nil
 }
 
