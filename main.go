fd92a8d0bcc69b2267f9fdb14cac5d9c7834c88f
igoogolx <igoogolx@gmail.com>
2023-07-30 01:04:30 +0800

    feat: add homeDir params

--- main.go
+++ main.go
@@ -24,13 +24,22 @@ var (
 func main() {
 	flag.BoolVar(&version, "version", false, "Print version")
 	flag.IntVar(&port, "port", constants.DefaultHubPort, "Running port, default:9000")
+	flag.StringVar(&homeDir, "home-dir", "", "Config dir, default: current dir")
 	flag.Parse()
+
+	if homeDir != "" {
+		if !filepath.IsAbs(homeDir) {
+			configDir, _ := os.UserConfigDir()
+			homeDir = filepath.Join(configDir, "itun2socks", homeDir)
+		}
+		constants.Path.SetHomeDir(homeDir)
+	}
+
+	configuration.SetConfigFilePath(constants.Path.ConfigFilePath())
 
 	currentDir, _ := os.Getwd()
 	homeDir = filepath.Join(currentDir)
-
 	constants.Path.SetHomeDir(homeDir)
-	configuration.SetConfigFilePath(constants.Path.ConfigFilePath())
 
 	if version {
 		fmt.Printf("version: %v, build on: %v", constants.Version, constants.BuildTime)
