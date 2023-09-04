d54aad55211f59385fa8947ea7cb4de6d7910053
igoogolx <igoogolx@gmail.com>
2023-09-04 15:03:29 +0800

    feat(log): format

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	cLog "github.com/Dreamacro/clash/log"
 	"github.com/igoogolx/itun2socks/internal/constants"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -29,4 +30,50 @@ func InitLog() {
 			Compress:   true, // disabled by default
 		},
 	})
+}
+
+type Prefix int64
+
+const (
+	InitPrefix Prefix = iota
+	ExecutorPrefix
+	DnsPrefix
+	ConfigurationPrefix
+	RulePrefix
+)
+
+func FormatLog(prefix Prefix, content string) string {
+	switch prefix {
+	case InitPrefix:
+		return "[INIT], " + content
+	case ExecutorPrefix:
+		return "[EXECUTOR], " + content
+	case DnsPrefix:
+		return "[DNS], " + content
+	case ConfigurationPrefix:
+		return "[CONFIGURATION], " + content
+	case RulePrefix:
+		return "[RULE], " + content
+	}
+	return "[UNKNOWN]"
+}
+
+func Infoln(format string, v ...any) {
+	cLog.Infoln(format, v)
+}
+
+func Warnln(format string, v ...any) {
+	cLog.Warnln(format, v)
+}
+
+func Errorln(format string, v ...any) {
+	cLog.Errorln(format, v)
+}
+
+func Debugln(format string, v ...any) {
+	cLog.Debugln(format, v)
+}
+
+func Fatalln(format string, v ...any) {
+	cLog.Fatalln(format, v)
 }
