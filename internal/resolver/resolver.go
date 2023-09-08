5e239225f71f578293617fdbfeee30e5e42bd2ee
igoogolx <igoogolx@gmail.com>
2023-09-08 13:14:57 +0800

    feat: resolver

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -1,46 +1,32 @@
 package resolver
 
 import (
-	"context"
 	cResolver "github.com/Dreamacro/clash/component/resolver"
 	"github.com/Dreamacro/clash/dns"
-	D "github.com/miekg/dns"
 	"strings"
 )
 
-type Client struct {
-	*dns.Resolver
-	withAnswer func(msg *D.Msg)
-}
-
-func (c Client) ExchangeContext(ctx context.Context, m *D.Msg) (msg *D.Msg, err error) {
-	if err == nil {
-		c.withAnswer(m)
-	}
-	return c.ExchangeContext(ctx, m)
-}
-
-func New(mainServer, defaultServer string, withAnswer func(msg *D.Msg)) (cResolver.Resolver, error) {
-	var boostNameResolver []dns.NameServer = nil
+func New(mainServer, defaultServer string) (cResolver.Resolver, error) {
+	var defaultNameResolver []dns.NameServer = nil
 	if len(defaultServer) != 0 {
-		boostNameResolver = []dns.NameServer{
+		defaultNameResolver = []dns.NameServer{
 			{
 				Net:  "tcp",
 				Addr: defaultServer,
 			},
 		}
 	}
-	localDnsNet := "tcp"
+	mainDnsNet := "tcp"
 	if strings.Contains(mainServer, "https") {
-		localDnsNet = "https"
+		mainDnsNet = "https"
 	}
-	localDnsClient := dns.NewResolver(dns.Config{
+	mainDnsClient := dns.NewResolver(dns.Config{
 		Main: []dns.NameServer{{
-			Net:  localDnsNet,
+			Net:  mainDnsNet,
 			Addr: mainServer,
 		}},
-		Default: boostNameResolver,
+		Default: defaultNameResolver,
 	})
 
-	return Client{localDnsClient, withAnswer}, nil
+	return mainDnsClient, nil
 }
