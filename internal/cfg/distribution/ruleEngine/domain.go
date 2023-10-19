1cdc43019218bb9f7ac9e8a97a15d49d7330597d
igoogolx <igoogolx@gmail.com>
2023-10-19 10:09:01 +0800

    fix: invalid dns

--- internal/cfg/distribution/ruleEngine/domain.go
+++ internal/cfg/distribution/ruleEngine/domain.go
@@ -11,8 +11,8 @@ type Domain struct {
 	policy  string
 }
 
-func (d Domain) Policy() constants.RuleConfig {
-	return constants.RuleConfig(d.policy)
+func (d Domain) Policy() constants.RuleType {
+	return constants.RuleType(d.policy)
 }
 
 func (d Domain) Type() constants.RuleConfig {
