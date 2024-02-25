5839ce56f2fe53d343aea4d2514f0e80aba3a74f
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-02-26 00:19:28 +0800

    fix: invalid default config dir

--- main.go
+++ main.go
@@ -21,22 +21,26 @@ var (
 	port    int
 )
 
+var packageName = "itun2socks"
+
 //go:generate go run scripts/generate.go
 func main() {
 	debug.SetMemoryLimit(32 * 1024 * 1024)
+
+	configDir, _ := os.UserConfigDir()
+	defaultHomeDir := filepath.Join(configDir, packageName)
+
 	flag.BoolVar(&version, "version", false, "Print version")
-	flag.IntVar(&port, "port", constants.DefaultHubPort, "Running port, default: ${user_config_dir}/itun2socks")
+	flag.IntVar(&port, "port", constants.DefaultHubPort, fmt.Sprintf("Running port, default: %v", defaultHomeDir))
 	flag.StringVar(&homeDir, "home_dir", "", "Config dir, default: current dir")
 	flag.Parse()
 
 	if homeDir != "" {
 		if !filepath.IsAbs(homeDir) {
-			configDir, _ := os.UserConfigDir()
-			homeDir = filepath.Join(configDir, "itun2socks", homeDir)
+			homeDir = filepath.Join(configDir, packageName, homeDir)
 		}
 	} else {
-		configDir, _ := os.UserConfigDir()
-		homeDir = filepath.Join(configDir, "itun2socks")
+		homeDir = filepath.Join(configDir, packageName)
 	}
 	_ = os.MkdirAll(homeDir, os.ModePerm)
 	constants.Path.SetHomeDir(homeDir)
