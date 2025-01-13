da54cc7ad918395d9e0845db7d747e882ce553e5
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-01-13 19:46:27 +0800

    v1.25.2 (#61)

--- scripts/generate.go
+++ scripts/generate.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	download("https://github.com/igoogolx/lux-rules/releases/download/v2.4.0/rules.tar.gz", filepath.Join("internal", "cfg", "distribution", "ruleEngine", "rules.tar.gz"))
-	download("https://github.com/igoogolx/lux-client/releases/download/v1.11.3/dist-ui.tar.gz", filepath.Join("api", "routes", "dist.tar.gz"))
+	download("https://github.com/igoogolx/lux-client/releases/download/v1.11.4/dist-ui.tar.gz", filepath.Join("api", "routes", "dist.tar.gz"))
 }
 
 func download(url string, outputPath string) {
