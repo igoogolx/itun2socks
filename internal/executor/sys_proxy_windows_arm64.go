228bc395114b8086f70ab323c387470b42dc8a50
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-03-08 13:19:21 +0800

    feat: support windows on arm

---
+++ internal/executor/sys_proxy_windows_arm64.go
@@ -0,0 +1,35 @@
+//go:build windows_arm64
+// +build windows_arm64
+
+package executor
+
+import (
+	"fmt"
+	"github.com/igoogolx/itun2socks/internal/cfg"
+	localserver "github.com/igoogolx/itun2socks/internal/local_server"
+	"sync"
+)
+
+type SysProxyDetail struct {
+	HubAddress string `json:"hubAddress"`
+}
+
+type SystemProxyClient struct {
+	sync.RWMutex
+	localserver localserver.Listener
+	config      *cfg.SystemProxyConfig
+	off         func() error
+}
+
+func (c *SystemProxyClient) RuntimeDetail(hubAddress string) (interface{}, error) {
+	return &SysProxyDetail{hubAddress}, fmt.Errorf("not implemented yet")
+}
+
+func (c *SystemProxyClient) Start() error {
+	return fmt.Errorf("not implemented yet")
+}
+
+func (c *SystemProxyClient) Close() error {
+	return fmt.Errorf("not implemented yet")
+
+}
