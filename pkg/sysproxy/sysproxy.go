f3ce577b98673ca5ac769f57a6a56ce8e584cf6a
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-03-24 19:57:17 +0800

    fix: system proxy can't work on windows

--- pkg/sysproxy/sysproxy.go
+++ pkg/sysproxy/sysproxy.go
@@ -7,25 +7,9 @@ func Set(addr string) error {
 	if err != nil {
 		return err
 	}
-	err = SetWebProxy(host, port)
-	if err != nil {
-		return err
-	}
-	err = SetSOCKSProxy(host, port)
-	if err != nil {
-		return err
-	}
-	return nil
+	return SetWebProxy(host, port)
 }
 
 func Clear() error {
-	err := DisableWebProxy()
-	if err != nil {
-		return err
-	}
-	err = DisableSOCKSProxy()
-	if err != nil {
-		return err
-	}
-	return nil
+	return DisableWebProxy()
 }
