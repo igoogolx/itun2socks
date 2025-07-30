e8c3bc8f3188af5d5883f5b0fb10be2f7ed278c7
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-07-30 23:46:27 +0800

    v1.31.0 (#101)
    * feat: light client mode
    
    * chore: upgrade dashboard
    
    * feat: make lightClientMode optional
    
    * refactor: reorder code
    
    * chore: upgrade dashboard

--- internal/configuration/config.go
+++ internal/configuration/config.go
@@ -34,6 +34,7 @@ type SettingCfg struct {
 	AutoConnect       bool   `json:"autoConnect,omitempty"`
 	AutoLaunch        bool   `json:"autoLaunch,omitempty"`
 	SensitiveInfoMode bool   `json:"sensitiveInfoMode,omitempty"`
+	LightClientMode   bool   `json:"lightClientMode,omitempty"`
 }
 
 type DnsServer struct {
