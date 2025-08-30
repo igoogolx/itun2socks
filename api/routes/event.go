c8ba995082dabbe890c29cb06b6e3db1292d4ecf
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-08-31 00:46:35 +0800

    feat: support event api

---
+++ api/routes/event.go
@@ -0,0 +1,195 @@
+package routes
+
+import (
+	"bytes"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/websocket"
+	"github.com/igoogolx/itun2socks/pkg/log"
+)
+
+type Client struct {
+	hub *Hub
+
+	conn *websocket.Conn
+
+	send chan []byte
+}
+
+type Hub struct {
+	clients map[*Client]bool
+
+	broadcast chan []byte
+
+	register chan *Client
+
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+const (
+	writeWait = 10 * time.Second
+
+	pongWait = 60 * time.Second
+
+	pingPeriod = (pongWait * 9) / 10
+
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		err := c.conn.Close()
+		if err != nil {
+			return
+		}
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	err := c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	if err != nil {
+		return
+	}
+	c.conn.SetPongHandler(func(string) error {
+		err := c.conn.SetReadDeadline(time.Now().Add(pongWait))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Debugln("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		log.Infoln(string(message))
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		err := c.conn.Close()
+		if err != nil {
+			return
+		}
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			err := c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err != nil {
+				return
+			}
+			if !ok {
+				err := c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				if err != nil {
+					return
+				}
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			_, err = w.Write(message)
+			if err != nil {
+				return
+			}
+
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				_, err = w.Write(newline)
+				if err != nil {
+					return
+				}
+				_, err = w.Write(<-c.send)
+				if err != nil {
+					return
+				}
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			err := c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err != nil {
+				return
+			}
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func serveEvent(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Debugln("error: %v", err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
+}
+
+var hub = newHub()
+
+func init() {
+	go hub.run()
+}
+
+func eventRouter() http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", serveEvent)
+	return r
+}
