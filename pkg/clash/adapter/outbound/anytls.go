abec345e3a25757cb547530ff39cfe0b13c3e4e1
igoogolx <27353191+igoogolx@users.noreply.github.com>
2026-07-14 17:21:19 +0800

    feat: init anytls of clash meta

--- pkg/clash/adapter/outbound/anytls.go
+++ pkg/clash/adapter/outbound/anytls.go
@@ -1,126 +1,139 @@
-package outbound
+package clashMeta
 
 import (
 	"context"
-	"fmt"
 	"net"
-	"net/netip"
 	"strconv"
+	"time"
 
-	anytls "github.com/anytls/sing-anytls"
-	"github.com/igoogolx/itun2socks/pkg/clash/component/dialer"
-	C "github.com/igoogolx/itun2socks/pkg/clash/constant"
-	"github.com/sagernet/sing/common/json/badoption"
-	singMetadata "github.com/sagernet/sing/common/metadata"
+	N "github.com/metacubex/mihomo/common/net"
+	"github.com/metacubex/mihomo/component/proxydialer"
+	C "github.com/metacubex/mihomo/constant"
+	"github.com/metacubex/mihomo/transport/anytls"
+	"github.com/metacubex/mihomo/transport/vmess"
+
+	M "github.com/metacubex/sing/common/metadata"
+	"github.com/metacubex/sing/common/uot"
+
+	metaOutbound "github.com/metacubex/mihomo/adapter/outbound"
 )
 
-type connContextKey string
-
-type AnyTls struct {
-	*Base
-	client        *anytls.Client
-	diaOutConnKey connContextKey
+type AnyTLS struct {
+	*metaOutbound.Base
+	client *anytls.Client
+	option *AnyTLSOption
 }
 
-type AnyTlsOption struct {
-	BasicOption
-	Name                     string             `proxy:"name"`
-	Server                   string             `proxy:"server"`
-	Port                     int                `proxy:"port"`
-	Password                 string             `proxy:"password,omitempty"`
-	IdleSessionCheckInterval badoption.Duration `proxy:"idle-session-check-interval,omitempty"`
-	IdleSessionTimeout       badoption.Duration `proxy:"idle-session-timeout,omitempty"`
-	MinIdleSession           int                `proxy:"min-idle-session,omitempty"`
-	UDP                      bool               `proxy:"udp,omitempty"`
+type AnyTLSOption struct {
+	metaOutbound.BasicOption
+	Name                     string                  `proxy:"name"`
+	Server                   string                  `proxy:"server"`
+	Port                     int                     `proxy:"port"`
+	Password                 string                  `proxy:"password"`
+	ALPN                     []string                `proxy:"alpn,omitempty"`
+	SNI                      string                  `proxy:"sni,omitempty"`
+	ECHOpts                  metaOutbound.ECHOptions `proxy:"ech-opts,omitempty"`
+	ClientFingerprint        string                  `proxy:"client-fingerprint,omitempty"`
+	SkipCertVerify           bool                    `proxy:"skip-cert-verify,omitempty"`
+	Fingerprint              string                  `proxy:"fingerprint,omitempty"`
+	Certificate              string                  `proxy:"certificate,omitempty"`
+	PrivateKey               string                  `proxy:"private-key,omitempty"`
+	UDP                      bool                    `proxy:"udp,omitempty"`
+	IdleSessionCheckInterval int                     `proxy:"idle-session-check-interval,omitempty"`
+	IdleSessionTimeout       int                     `proxy:"idle-session-timeout,omitempty"`
+	MinIdleSession           int                     `proxy:"min-idle-session,omitempty"`
 }
 
-func (at *AnyTls) StreamConn(c net.Conn, metadata *C.Metadata) (net.Conn, error) {
-
-	if at.client == nil {
-		return nil, fmt.Errorf("invalid AnyTls client")
-	}
-
-	dest := singMetadata.Socksaddr{
-		Port: uint16(metadata.DstPort),
-	}
-
-	if len(metadata.Host) != 0 {
-		dest.Fqdn = metadata.Host
-	} else {
-
-		addr, err := netip.ParseAddr(metadata.DstIP.String())
-
-		if err != nil {
-
-			return nil, fmt.Errorf("invalid addr for AnyTls client: %s, error: %s", addr, err)
-		}
-		dest.Addr = addr
-
-	}
-
-	return at.client.CreateProxy(context.WithValue(context.Background(), at.diaOutConnKey, c), dest)
-}
-
-func (at *AnyTls) DialContext(ctx context.Context, metadata *C.Metadata, opts ...dialer.Option) (_ C.Conn, err error) {
-
-	c, err := dialer.DialContext(ctx, "tcp", at.addr, at.Base.DialOptions(opts...)...)
+func (t *AnyTLS) DialContext(ctx context.Context, metadata *C.Metadata) (_ C.Conn, err error) {
+	c, err := t.client.CreateProxy(ctx, M.ParseSocksaddrHostPort(metadata.String(), metadata.DstPort))
 	if err != nil {
-		return nil, fmt.Errorf("%s connect error: %w", at.addr, err)
+		return nil, err
 	}
-	tcpKeepAlive(c)
+	return NewConn(c, t), nil
+}
 
-	//defer func(c net.Conn) {
-	//	safeConnClose(c, err)
-	//}(c)
+func (t *AnyTLS) ListenPacketContext(ctx context.Context, metadata *C.Metadata) (_ C.PacketConn, err error) {
+	if err = t.ResolveUDP(ctx, metadata); err != nil {
+		return nil, err
+	}
 
-	c, err = at.StreamConn(c, metadata)
+	// create tcp
+	c, err := t.client.CreateProxy(ctx, uot.RequestDestination(2))
 	if err != nil {
 		return nil, err
 	}
 
-	return NewConn(c, at), nil
-
+	// create uot on tcp
+	destination := M.SocksaddrFromNet(metadata.UDPAddr())
+	return NewPacketConn(N.NewThreadSafePacketConn(uot.NewLazyConn(c, uot.Request{Destination: destination})), t), nil
 }
 
-func (at *AnyTls) dialOut(ctx context.Context) (net.Conn, error) {
-
-	if v, ok := ctx.Value(at.diaOutConnKey).(net.Conn); ok {
-		return v, nil
-	}
-
-	return nil, fmt.Errorf("no conn found")
+// SupportUOT implements C.ProxyAdapter
+func (t *AnyTLS) SupportUOT() bool {
+	return true
 }
 
-func NewAnyTls(option AnyTlsOption) *AnyTls {
+// ProxyInfo implements C.ProxyAdapter
+func (t *AnyTLS) ProxyInfo() C.ProxyInfo {
+	info := t.Base.ProxyInfo()
+	info.DialerProxy = t.option.DialerProxy
+	return info
+}
 
-	ctx := context.Background()
+// Close implements C.ProxyAdapter
+func (t *AnyTLS) Close() error {
+	return t.client.Close()
+}
 
+func NewAnyTLS(option AnyTLSOption) (*AnyTLS, error) {
 	addr := net.JoinHostPort(option.Server, strconv.Itoa(option.Port))
-
-	anyTlsClient := &AnyTls{
-
-		Base: &Base{
-			name:  option.Name,
-			addr:  addr,
-			tp:    C.AnyTls,
-			udp:   option.UDP,
-			iface: option.Interface,
-			rmark: option.RoutingMark,
-		},
-		diaOutConnKey: connContextKey("connKey"),
+	outbound := &AnyTLS{
+		Base: metaOutbound.NewBase(metaOutbound.BaseOption{
+			Name:         option.Name,
+			Addr:         addr,
+			Type:         C.AnyTLS,
+			ProviderName: option.ProviderName,
+			UDP:          option.UDP,
+			TFO:          option.TFO,
+			MPTCP:        option.MPTCP,
+			Interface:    option.Interface,
+			RoutingMark:  option.RoutingMark,
+			Prefer:       option.IPVersion,
+		}),
+		option: &option,
 	}
+	outbound.dialer = option.NewDialer(outbound.DialOptions())
+	singDialer := proxydialer.NewSingDialer(outbound.dialer)
 
-	client, err := anytls.NewClient(ctx, anytls.ClientConfig{
+	tOption := anytls.ClientConfig{
 		Password:                 option.Password,
-		IdleSessionCheckInterval: option.IdleSessionCheckInterval.Build(),
-		IdleSessionTimeout:       option.IdleSessionTimeout.Build(),
+		Server:                   M.ParseSocksaddrHostPort(option.Server, uint16(option.Port)),
+		Dialer:                   singDialer,
+		IdleSessionCheckInterval: time.Duration(option.IdleSessionCheckInterval) * time.Second,
+		IdleSessionTimeout:       time.Duration(option.IdleSessionTimeout) * time.Second,
 		MinIdleSession:           option.MinIdleSession,
-		DialOut:                  anyTlsClient.dialOut,
-	})
-	if err == nil {
-		anyTlsClient.client = client
 	}
+	echConfig, err := option.ECHOpts.Parse()
+	if err != nil {
+		return nil, err
+	}
+	tlsConfig := &vmess.TLSConfig{
+		Host:              option.SNI,
+		SkipCertVerify:    option.SkipCertVerify,
+		NextProtos:        option.ALPN,
+		FingerPrint:       option.Fingerprint,
+		Certificate:       option.Certificate,
+		PrivateKey:        option.PrivateKey,
+		ClientFingerprint: option.ClientFingerprint,
+		ECH:               echConfig,
+	}
+	if tlsConfig.Host == "" {
+		tlsConfig.Host = option.Server
+	}
+	tOption.TLSConfig = tlsConfig
 
-	return anyTlsClient
+	client := anytls.NewClient(context.TODO(), tOption)
+	outbound.client = client
 
+	return outbound, nil
 }
