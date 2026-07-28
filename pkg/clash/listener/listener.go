ea28a763a6c4df646dc3c85895b19fcdd53cdd76
igoogolx <27353191+igoogolx@users.noreply.github.com>
2026-07-28 11:49:38 +0800

    refactor: remove unused files

--- pkg/clash/listener/listener.go
+++ pkg/clash/listener/listener.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"net"
 	"strconv"
-	"strings"
 	"sync"
 
 	"github.com/igoogolx/itun2socks/pkg/clash/adapter/inbound"
-	"github.com/igoogolx/itun2socks/pkg/clash/config"
 	C "github.com/igoogolx/itun2socks/pkg/clash/constant"
 	"github.com/igoogolx/itun2socks/pkg/clash/listener/http"
 	"github.com/igoogolx/itun2socks/pkg/clash/listener/mixed"
@@ -192,95 +190,6 @@ func reCreateListeners(inbounds []C.Inbound, tcpIn chan<- C.ConnContext, udpIn c
 	}
 	for _, inbound := range needCreate {
 		createListener(inbound, tcpIn, udpIn)
-	}
-}
-
-func PatchTunnel(tunnels []config.Tunnel, tcpIn chan<- C.ConnContext, udpIn chan<- *inbound.PacketAdapter) {
-	tunnelMux.Lock()
-	defer tunnelMux.Unlock()
-
-	type addrProxy struct {
-		network string
-		addr    string
-		target  string
-		proxy   string
-	}
-
-	tcpOld := lo.Map(
-		lo.Keys(tunnelTCPListeners),
-		func(key string, _ int) addrProxy {
-			parts := strings.Split(key, "/")
-			return addrProxy{
-				network: "tcp",
-				addr:    parts[0],
-				target:  parts[1],
-				proxy:   parts[2],
-			}
-		},
-	)
-	udpOld := lo.Map(
-		lo.Keys(tunnelUDPListeners),
-		func(key string, _ int) addrProxy {
-			parts := strings.Split(key, "/")
-			return addrProxy{
-				network: "udp",
-				addr:    parts[0],
-				target:  parts[1],
-				proxy:   parts[2],
-			}
-		},
-	)
-	oldElm := lo.Union(tcpOld, udpOld)
-
-	newElm := lo.FlatMap(
-		tunnels,
-		func(tunnel config.Tunnel, _ int) []addrProxy {
-			return lo.Map(
-				tunnel.Network,
-				func(network string, _ int) addrProxy {
-					return addrProxy{
-						network: network,
-						addr:    tunnel.Address,
-						target:  tunnel.Target,
-						proxy:   tunnel.Proxy,
-					}
-				},
-			)
-		},
-	)
-
-	needClose, needCreate := lo.Difference(oldElm, newElm)
-
-	for _, elm := range needClose {
-		key := fmt.Sprintf("%s/%s/%s", elm.addr, elm.target, elm.proxy)
-		if elm.network == "tcp" {
-			tunnelTCPListeners[key].Close()
-			delete(tunnelTCPListeners, key)
-		} else {
-			tunnelUDPListeners[key].Close()
-			delete(tunnelUDPListeners, key)
-		}
-	}
-
-	for _, elm := range needCreate {
-		key := fmt.Sprintf("%s/%s/%s", elm.addr, elm.target, elm.proxy)
-		if elm.network == "tcp" {
-			l, err := tunnel.New(elm.addr, elm.target, elm.proxy, tcpIn)
-			if err != nil {
-				log.Errorln("Start tunnel %s error: %s", elm.target, err.Error())
-				continue
-			}
-			tunnelTCPListeners[key] = l
-			log.Infoln("Tunnel(tcp/%s) proxy %s listening at: %s", elm.target, elm.proxy, tunnelTCPListeners[key].Address())
-		} else {
-			l, err := tunnel.NewUDP(elm.addr, elm.target, elm.proxy, udpIn)
-			if err != nil {
-				log.Errorln("Start tunnel %s error: %s", elm.target, err.Error())
-				continue
-			}
-			tunnelUDPListeners[key] = l
-			log.Infoln("Tunnel(udp/%s) proxy %s listening at: %s", elm.target, elm.proxy, tunnelUDPListeners[key].Address())
-		}
 	}
 }
 
